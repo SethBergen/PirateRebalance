73e825fd3e4a7d6686356a1c7f0552cdf2883e1a
SethBergen <sbergen@ualberta.ca>
2020-05-11 22:06:44 -0600

    Initial Commit

---
+++ src/pirate_rebalance/plugins/PirateRebalancePlugin.java
@@ -0,0 +1,19 @@
+package pirate_rebalance.plugins;
+
+import com.fs.starfarer.api.BaseModPlugin;
+import com.fs.starfarer.api.Global;
+
+import org.apache.log4j.Logger;
+
+public class PirateRebalancePlugin extends BaseModPlugin {
+    public static Logger log;
+
+    {
+        log = Global.getLogger(PirateRebalancePlugin.class);
+    }
+
+    @Override
+    public void onGameLoad(boolean newGame){
+        log.info("PIRATE REBALANCE LOADED");
+    }
+}
